f29cb113549a46e2279f4af7f7ab148404e881ee
Kyle Jones <kyle.jones2893@gmail.com>
2023-10-30 13:14:15 -0400

    added rest edpoints for students

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,15 +1,16 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(path="api/v1/student")
 public class StudentController {
+
 
     private final StudentService studentService;
 
@@ -18,9 +19,33 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-
     @GetMapping
     public List<Student> getStudents() {
         return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        try {
+            studentService.addNewStudent(student);
+        }
+        catch (IllegalStateException ise) {
+            throw new ResponseStatusException(
+                    HttpStatusCode.valueOf(409), "User with that email already exists", ise
+            );
+        }
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
+        studentService.updateStudent(studentId, name, email);
     }
 }
